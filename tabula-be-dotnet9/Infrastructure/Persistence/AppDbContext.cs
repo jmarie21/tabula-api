b129d19142fe90ad6eaf82d9f2352c11425a680c
jmarie21 <johnmarieygot21@gmail.com>
2025-08-27 09:52:21 +0800

    feat: add db context, user entity and initial migration

---
+++ tabula-be-dotnet9/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tabula_be_dotnet9.Domain.Entities;
+
+namespace tabula_be_dotnet9.Infrastructure.Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
